11ccd1ea0a811c3621937ee61d14eb62edf67661
Laffy <huuhoag1412@gmail.com>
2022-03-11 21:00:51 +0700

    create a BookNote struct

--- main.cpp
+++ main.cpp
@@ -1,31 +1,23 @@
 #include <iostream>
 #include <string>
 #include <cmath>
+using namespace std;
+struct Book
+{
+    string name;
+    string authorName;
+    string publisher;
+    int yearOfPublication;
+    bool status;
+};
+struct BookNode
+{
+    Book *book;
+    BookNode *nextNode;
+};
 
 using namespace std;
 
 int main()
 {
-    int n;
-    cin >> n;
-    int a[n];
-    for (int i = 0; i < n; i++)
-    {
-        cin >> a[i];
-    }
-    int max = a[0];
-    int min = a[0];
-    for (int i = 1; i < n; i++)
-    {
-        if (a[i] > max)
-        {
-            max = a[i];
-        }
-        if (a[i] < min)
-        {
-            min = a[i];
-        }
-    }
-    cout << max - min;
-    return 0;
 }
